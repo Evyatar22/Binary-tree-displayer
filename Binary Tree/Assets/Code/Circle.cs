d849ac06d8a401173d063970c1a37b632b2d816e
derHugo <hugo.franz2@web.de>
2022-03-01 11:46:34 +0100

    Rectoring suggestions and fix for tree cration and display - derHugo

---
+++ Binary Tree/Assets/Code/Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Circle : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private RectTransform rectTransform;
+    [SerializeField] private TMP_Text text;
+
+    public Image Image
+    {
+        get
+        {
+            if (image) return image;
+
+            image = GetComponent<Image>();
+
+            return image;
+        }
+    }
+
+    public RectTransform RectTransform
+    {
+        get
+        {
+            if (rectTransform) return rectTransform;
+
+            rectTransform = GetComponent<RectTransform>();
+
+            return rectTransform;
+        }
+    }
+
+    public TMP_Text Text
+    {
+        get
+        {
+            if (text) return text;
+
+            text = GetComponentInChildren<TMP_Text>();
+
+            return text;
+        }
+    }
+}
